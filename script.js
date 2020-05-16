0fc21ed3be73a1d9dabd203144eb7f337a21315b
Matthew <mcraig150@gmail.com>
2020-05-16 11:42:18 -0400

    Added more comments

--- script.js
+++ script.js
@@ -6,12 +6,19 @@ var btn3 = document.querySelector("#btn3");
 
 var qst = document.querySelector("#qst");
 
+
+//questions for the quiz
 var questions = "what do you end your lines with in javascript? What does != mean? What is a variable? what is a boolean? what does || mean? What is the first thing in a html document? How do you define classes in css? What function do you use to turn a string into an int? What is the latest version of html? What tag do you use to add a link to the page?"
 
+
+//creates an array of questions
 var quest = questions.split('?');
 
+
+//array of answers to be put on buttons
 var answers = ["Semicolon. Period. Comma.", "Equals. Does not equal. True.", "A number. A string. Both.", "A function for adding. A way of commenting. A true false variable.", "Or. And. False.", "<!DOCTYPE>. <footer>. <head>.", "With a period. With a hashtag. With a tilda.", "parseInt. toNum. toInt.", "9. 11. 5", "<img>. <a>. <link>" ]
 
+//array of correct anwsers
 var correct =[1, 2, 3, 3, 1, 1, 1, 1, 3, 2];
 
 var wrong = false;
@@ -46,9 +53,11 @@ function timer() {
 
     var time = document.querySelector(".timer");
 
+
+    //starts timer
     var countdown = setInterval (function(){
         
-        
+        //Checks to see if there is time left
         if (timeLeft <= 0 || stop ===  true) {
             clearInterval(countdown);
             time.textContent = "Finished!";
@@ -58,6 +67,7 @@ function timer() {
             time.textContent = "Seconds remaining: " + timeLeft;
         }
 
+        //checks to see if anwser was incorrect and subtracts time if it was
         if(wrong === true) {
             timeLeft -=  10;
             wrong = false;
@@ -184,45 +194,47 @@ function update() {
 
 
 }
+
+//function used for scores
 function scores() {
 
     //Adding scores to scoreboard
     qjumbo.style.display = "none";
     sjumbo.style.display = "block";
 
+
+    //listening for click on form
     form.addEventListener("click", function(){
+        //stops defualt of refresh on click
         event.preventDefault();
 
     });
 
 
-
+    //listening for click of submit button
     sub.addEventListener("click", function(){
 
     
-     
-
-
-
-
         var ul = document.getElementById("scores");
         
-
+        //creates variable based on value of the form
         var me = document.getElementById("name").value;
        
 
-        
-
-
+        //checks for previous scores and setts the points array to them if there was
         if(localStorage.getItem("scored") != undefined){
 
         points = localStorage.getItem("scored").split(",");
 
         }
 
+
+        //adds current user and score to the points array
         points.push(me + " : " + score);
         
 
+
+        //for loop to add scores to the UL of the page
         for(var i = 0; i < points.length; i++){
             var li = document.createElement("li");
             li.appendChild(document.createTextNode(points[i] + " " ));
@@ -231,8 +243,11 @@ function scores() {
         }
         
 
+        // removes the text feild
         form.style.display = "none";
         
+
+        //sets the localstorage to the array for future use
         localStorage.setItem("scored", points)
 
     });
